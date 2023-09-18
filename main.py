1149ecd92b43737d7abf02dd0bfa134581f418a3
hongjie.zhang <zhanghongjie@momenta.ai>
2023-09-18 12:16:22 +0800

    update file read

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def west_pp():
     data = 'this is data'
     for i in range(5):
         pprint(i)
-        if i%2 != 0:
-            break
+        if i % 2 != 0:
+            pprint(f'{i} is 偶数')
 
 
 west_pp()
