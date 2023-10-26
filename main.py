b26def1ea4aa78af7499bfb26e540970c93099a1
hongjie.zhang <hongjie.zhang@momenta.ai>
2023-10-26 11:20:50 +0800

    2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pprint import pprint
 
 def west_pp():
     data = 'this is data'
-    for i in range(5):
+    for i in range(len(data)):
         pprint(i)
         if i % 2 != 0:
             pprint(f'{i} is 偶数')
